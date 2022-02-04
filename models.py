657a9c53a2b54fa6f073f5a041c84a59d8917bee
Lidan-Huang <huangld0314@gmail.com>
2022-02-04 11:32:46 -0800

    finished step1

--- models.py
+++ models.py
@@ -13,3 +13,18 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
+
+class Pet(db.Model):
+    """Pet docstring"""
+    __table_name__ = "pets"
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(50), nullable=False)   
+    species = db.Column(db.String(100), nullable=False) 
+    photo_url = db.Column(db.Text, default='', nullable=False)
+    age = db.Column(db.Text, nullable=False)
+    notes = db.Column(db.Text, nullable=True)
+    available = db.Column(db.Boolean, default=True, nullable=False)
