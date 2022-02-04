e1d5fc19857d73e6a6a7e3ab12581b0862c24074
Lidan-Huang <huangld0314@gmail.com>
2022-02-04 12:34:37 -0800

    working on pet_add_form

--- forms.py
+++ forms.py
@@ -1,1 +1,14 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import InputRequired, Optional
+
+class AddPetForm(FlaskForm):
+    """Form to add a pet."""
+    name = StringField("Pet Name")
+    species = StringField("Species")
+    photo_url = StringField("Photo URL")
+    age = StringField("Age")
+    notes = StringField("Notes")
+
+
