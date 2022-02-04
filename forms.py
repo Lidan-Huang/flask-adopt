3219669cdf596c351d472de11a8bf36428609537
Lidan-Huang <huangld0314@gmail.com>
2022-02-04 15:38:32 -0800

    working on edit pet form

--- forms.py
+++ forms.py
@@ -3,20 +3,26 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField
 from wtforms.validators import InputRequired, Optional, URL
 
+
 class AddPetForm(FlaskForm):
     """Form to add a pet."""
     name = StringField("Pet Name",
-                        validators=[InputRequired()])
-    species = StringField("Species",
-                        validators=[InputRequired()])
+                       validators=[InputRequired()])
+    species = SelectField("Species",
+                          validators=[InputRequired()],
+                          choices=[('cat', 'Cat'),
+                                   ('dog', 'Dog'),
+                                   ('porcupine', 'Porcupine')])
     photo_url = StringField("Photo URL",
-                        validators=[Optional(), URL()])
+                            validators=[Optional(), URL()])
 
-    age = SelectField('Age', 
-                        choices=[('baby', 'Baby'),
-                                ('young', 'Young'),
-                                ('adult', 'Adult'),
-                                ('senior', 'Senior')])
+    age = SelectField('Age',
+                      choices=[('baby', 'Baby'),
+                               ('young', 'Young'),
+                               ('adult', 'Adult'),
+                               ('senior', 'Senior')])
     notes = StringField("Notes")
 
 
+class EditPetForm(FlaskForm):
+    """Edit already showed pet"""
